8f9c3ee392a2f098d87bef12722ae1c199a19d4c
Famil <frostgamingrespawn@gmail.com>
2020-09-27 21:36:02 +0200

    added space

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -22,6 +22,7 @@ class WelcomeViewController: UIViewController {
         super.viewWillDisappear(animated);
         navigationController?.isNavigationBarHidden = false;
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
